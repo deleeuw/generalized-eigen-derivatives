5b84592f96ee47b3ff7ecc6c761c9da11cd2d175
Jan de Leeuw <jan@deleeuwpdx.net>
2025-07-07 13:07:07 -0400

    lin/nonlin

---
+++ gevNonlinear.R
@@ -0,0 +1,23 @@
+dsA <- function(theta, ...) {
+  # This routine returns a matrix D_sA
+}
+
+dsB <- function(theta, ...) {
+  # This routine returns a matrix D_sB
+}
+
+dstA <- function(theta, ...) {
+  # This routine returns a n x n symmetric matrix with elements D_{st}a_{ij}
+}
+
+dstB <- function(theta, ...) {
+  # This routine returns an array D_{st}B
+}
+
+theA <- function(theta, ...) {
+  # This routine return the matrix A
+}
+
+theB <- function(theta, ...) {
+  # This routine return the matrix B
+}
