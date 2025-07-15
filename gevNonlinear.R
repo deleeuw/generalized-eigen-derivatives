bba436dbc88f048eefb1d524716afee4c7f8ebcb
Jan de Leeuw <jan@deleeuwpdx.net>
2025-07-15 11:12:10 -0400

    bugs

--- gevNonlinear.R
+++ gevNonlinear.R
@@ -21,7 +21,6 @@ gevNonlinear <- function(theta) {
   dl <- matrix(0, n, p)
   dx <- array(0, c(n, p, n))
   ddl <- array(0, c(p, p, n))
-  ddx <- array(0, c(p, p, n, n))
   for (i in 1:n) {
     xi <- x[, i]
     li <- l[i]
@@ -35,6 +34,30 @@ gevNonlinear <- function(theta) {
       dl[i, s] <- sum(xi * (dsab %*% xi))
       dx[, s, i] <- -wi %*% dsab %*% xi - 0.5 * sum(xi * (dsb %*% xi)) * xi
     }
+  }
+  ddl <- gevHessianValues(theta, x, l)
+  ddx <- gevHessianVectors(theta, x, l, dx, dl)
+  return(list(
+    a = a,
+    b = b,
+    l = l,
+    x = x,
+    dl = dl,
+    dx = dx,
+    ddl = ddl,
+    ddx = ddx
+  ))
+}
+
+
+gevHessianValues <- function(theta, x, l) {
+  ddl <- array(0, c(p, p, n))
+  for (i in 1:n) {
+    xi <- x[, i]
+    li <- l[i]
+    mpl <- l - li
+    mpl <- diag(ifelse(mpl == 0, 0, 1 / mpl))
+    wi <- x %*% mpl %*% t(x)
     for (s in 1:p) {
       dsa <- dA(theta, s)
       dsb <- dB(theta, s)
@@ -44,42 +67,62 @@ gevNonlinear <- function(theta) {
         dtb <- dB(theta, t)
         dtab <- dta - li * dtb
         ddtp <- ddA(theta, s, t) - li * ddB(theta, s, t)
-        dtxi <- dx[, t, i]
         accu <- -2 * sum(xi * (dsab %*% wi %*% dtab %*% xi))
         accu <- accu + sum(xi * (ddtp %*% xi))
         accu <- accu - sum(xi * (dtb %*% xi)) * sum(xi * (dsab %*% xi))
         accu <- accu - sum(xi * (dsb %*% xi)) * sum(xi * (dtab %*% xi))
         ddl[s, t, i] <- accu
-        dtwi <- 0
-        for (j in 1:n) {
-          if (j != i) {
-            xj <- x[, j]
-            lj <- l[j]
-            dtxj <- dx[, t, j]
-            dtwi <- dtwi + (outer(xj, dtxj) + outer(dtxj, xj)) / (lj - li)
-            dtwi <- dtwi - ((dl[j, t] - dl[i, t]) / ((lj - li)^2)) * outer(xj, xj)
-          }
-        }
-        accu <- 0
-        accu <- accu - dtwi %*% dsab %*% xi
-        accu <- accu - wi %*% ddtp %*% xi
-        accu <- accu + dl[i, t] * wi %*% dsb %*% xi
-        accu <- accu - wi %*% dsab %*% dtxi
-        accu <- accu - sum(dtxi * (dsb %*% xi)) * xi
-        accu <- accu - 0.5 * sum(xi * (dsb %*% xi)) * dtxi
-        accu <- accu - 0.5 * sum(xi * (ddB(theta, s, t) %*% xi)) * xi
-        ddx[s, t, , i] <- accu
       }
     }
   }
-  return(list(
-    values = l,
-    vectors = x,
-    dvalues = dl,
-    dvectors = dx,
-    ddvalues = ddl,
-    ddvectors = ddx
-  ))
+  return(ddl)
+}
+
+gevHessianVectors <- function(theta, x, l, dx, dl) {
+  ddx <- array(0, c(p, p, n, n))
+  for (i in 1:n) {
+    xi <- x[, i]
+    li <- l[i]
+    for (s in 1:p) {
+      dsa <- dA(theta, s)
+      dsb <- dB(theta, s)
+      dsab <- dsa - li * dsb
+      for (t in 1:p) {
+        dta <- dA(theta, t)
+        dtb <- dB(theta, t)
+        dtab <- dta - li * dtb
+        dstb <- ddB(theta, s, t)
+        ddtp <- ddA(theta, s, t) - li * dstb
+        dtxi <- dx[, t, i]
+        dtli <- dl[i, t]
+        accu <- 0
+        accu <- accu + sum(xi * (dsb %*% xi)) * dtxi / 2
+        accu <- accu + sum(xi * (dstb %*% xi)) * xi / 2
+        accu <- accu + sum(xi * (dsb %*% dtxi)) * xi
+        for (j in 1:n) {
+          if (j == i) {
+            next
+          }
+          xj <- x[, j]
+          dtlj <- dl[j, t]
+          lj <- l[j]
+          dlij <- lj - li
+          dtxj <- dx[, t, j]
+          djab <- sum(xj * (dsab %*% xi))
+          accj <- 0
+          accj <- accj + sum(dtxj * (dsab %*% xi))
+          accj <- accj + sum(xj * (dsab %*% dtxi))
+          accj <- accj + sum(xj * (ddtp %*% xi))
+          accj <- accj - dtli * sum(xj * (dsb %*% xi))
+          accu <- accu + (accj / dlij) * xj
+          accu <- accu - ((dtlj - dtli) / (dlij^2)) * djab * xj
+          accu <- accu + (djab / dlij) * dtxj
+        }
+        ddx[s, t, , i] <- -accu
+      }
+    }
+  }
+  return(ddx)
 }
 
 gevNonlinearNum <- function(theta) {
@@ -105,7 +148,9 @@ gevNonlinearNum <- function(theta) {
     h <- myGeigen(a, b)
     return(h$vectors[ii, ss])
   }
-  h <- myGeigen(theA(theta), theB(theta))
+  a <- theA(theta)
+  b <- theB(theta)
+  h <- myGeigen(a, b)
   l <- h$values
   x <- h$vectors
   dl <- jacobian(func = theFunc, x = theta, ss = 0)
@@ -138,12 +183,17 @@ gevNonlinearNum <- function(theta) {
       )
     }
   }
-  return(list(
-    values = l,
-    vectors = x,
-    dvalues = dl,
-    dvectors = dx,
-    ddvalues = ddl,
-    ddvectors = ddx
-  ))
+  return(
+    list(
+      a = a,
+      b = b,
+      l = l,
+      x = x,
+      dl = dl,
+      dx = dx,
+      ddl = ddl,
+      ddx = ddx
+    )
+  )
 }
+
