b77c00e57a05ec637bbe37ac98323c3069bde75e
Jan de Leeuw <jan@deleeuwpdx.net>
2025-07-25 17:58:12 -0400

    gevd

---
+++ gsvdTemplate.R
@@ -0,0 +1,36 @@
+theF <- function(theta) {
+  
+}
+
+theG <- function(theta) {
+  
+}
+
+theH <- function(theta) {
+  
+}
+
+df <- function(theta) {
+  
+}
+
+dg <- function(theta) {
+  
+}
+
+dh <- function(theta) {
+  
+}
+
+ddf <- function(theta) {
+  
+}
+
+ddg <- function(theta) {
+  
+}
+
+ddh <- function(theta) {
+  
+}
+
