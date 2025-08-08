257c0134d6e6255c248983e75cf56d4c8a92ddc4
Jan de Leeuw <jan@deleeuwpdx.net>
2025-08-07 20:06:20 -0400

    dumb error

--- mdfa.R
+++ mdfa.R
@@ -1,37 +1,180 @@
 set.seed(12345)
+library(numDeriv)
 data(cattell, package = "psych")
 cmat <- cattell
-a <- .5 * cbind(matrix(rnorm(24), 12, 2), diag(12))
-pattern <- cbind(matrix(1, 12, 2), diag(12))
-library(numDeriv)
+r <- 3
+m <- 12
+n <- 15
+p <- m * (r + 1)
+nfix <- 0
 
+theta <- (1:48) / 100
 
-mdfaDerivatives <- function(a, cmat, pattern) {
-  n <- nrow(cmat)
-  aca <- crossprod(a, cmat %*% a)
-  hca <- eigen(aca)
-  hvec <- hca$vectors[, 1:n]
-  hval <- diag(1 / sqrt(hca$values[1:n]))
-  ac2 <- hvec %*% hval %*% t(hvec)
-  g <- pattern * (cmat %*% a %*% ac2)
-  return(g)
-}
+fix <- matrix(c(1, 2, 0, 1, 3, 0, 2, 3, 0), 3, 3, byrow = TRUE)
 
-mdfaNumDerivatives <- function(a, cmat) {
-  p <- ncol(a) - nrow(a)
-  aa <- c(as.vector(a[, 1:p]), diag(a[, -(1:p)]))
-  n <- nrow(cmat)
-  p <- (length(aa) / n) - 1
-  theFunc <- function(aa) {
-    aa <- cbind(matrix(aa[1:(n * p)], n , p), diag(aa[-(1:(n * p))]))
-    ad <- crossprod(aa, cmat %*% aa)
-    lb <- abs(eigen(ad)$values)
-    return(sum(sqrt(lb)))
+indi <- function(s) {
+  if (s > (m * r)) {
+    i <- s - (m * r)
+    j <- r + i
+  } else {
+    j <- ceiling(s / m)
+    i <- s - (j - 1) * m
   }
-  g <- grad(theFunc, aa)
-  h <- hessian(theFunc, aa)
-  g <- cbind(matrix(g[1:(n * p)], n , p), diag(g[-(1:(n * p))]))
-  return(list(g = g, h = h))
+  return(c(i, j))
+}
+
+jndi <- function(i, j) {
+  return(i + (j - 1) * m)
+}
+
+ei <- function(i, n) {
+  return(ifelse(i == 1:n, 1, 0))
 }
 
 
+theT <- function(theta) {
+  tmat <- matrix(0, m, m + r)
+  for (s in 1:p) {
+    if (s <= (m * r)) {
+      ij <- indi(s)
+      i <- ij[1]
+      j <- ij[2]
+    } else {
+      i <- s - (m * r)
+      j <- i + r
+    }
+    tmat[i, j] <- theta[s]
+  }
+  if (nfix > 0) {
+    for (k in 1:nfix) {
+      tmat[fix[k, 1], fix[k, 2]] <- fix[k, 3]
+    }
+  }
+  return(tmat)
+}
+
+dT <- function(theta, s) {
+  if (s <= (m * r)) {
+    ij <- indi(s)
+    i <- ij[1]
+    j <- ij[2]
+  } else {
+    i <- s - (m * r)
+    j <- i + r
+  }
+  return(outer(ei(i, m), ei(j, n)))
+}
+
+theA <- function(theta) {
+  tmat <- theT(theta)
+  return(crossprod(tmat, cmat %*% tmat))
+}
+
+dA <- function(theta, s) {
+  tmat <- theT(theta)
+  ts <- dT(theta, s)
+  qs <- crossprod(ts, cmat %*% tmat)
+  qs <- qs + t(qs)
+  return(qs)
+}
+
+mdfaEigenDerivatives <- function(theta) {
+  amat <- theA(theta)
+  heig <- eigen(amat, symmetric = TRUE)
+  hvec <- heig$vectors
+  hval <- heig$values
+  hinv <- 1 / sqrt(abs(hval))
+  f <- sum(sqrt(hval[1:m]))
+  g <- matrix(0, m, p)
+  h <- array(0, c(m, p, p))
+  halt <- array(0, c(m, p, p))
+  for (nu in 1:m) {
+    xnu <- hvec[, nu]
+    lnu <- hval[nu]
+    gil <- diag(ifelse((hval - lnu) == 0, 0, 1 / (hval - lnu)))
+    ainv <- hvec %*% gil %*% t(hvec)
+    for (s in 1:p) {
+      ts <- dT(theta, s)
+      qs <- dA(theta, s)
+      g[nu, s] <- sum(xnu * qs %*% xnu)
+      for (t in 1:p) {
+        tt <- dT(theta, t)
+        qt <- dA(theta, t)
+        hsum <- 0
+        for (eta in 1:n) {
+          xeta <- hvec[, eta]
+          leta <- hval[eta]
+          if (eta == nu) {
+            next
+          }
+          hsum <- hsum + sum(xnu * (qs %*% xeta)) * sum(xnu * (qt %*% xeta)) / (leta - lnu)
+        }
+        qst <- crossprod(ts, cmat %*% tt)
+        qst <- qst + t(qst)
+        h[nu, s, t] <- -2 * hsum + sum(xnu * (qst %*% xnu))
+      }
+    }
+  }
+  return(list(f = f, dl = g, ddl = h))
+}
+
+mdfaDerivatives <- function(theta) {
+  amat <- theA(theta)
+  heig <- eigen(amat, symmetric = TRUE)
+  hvec <- heig$vectors[, 1:m]
+  hval <- heig$values[1:m]
+  hinv <- 1 / sqrt(abs(hval))
+  h <- mdfaEigenDerivatives(theta)
+  ds <- drop(hinv %*% h$dl) / 2
+  dds <- matrix(0, p, p)
+  for (nu in 1:m) {
+    dds <- dds - ((hval[nu]^(-3 / 2)) * outer(h$dl[nu, ], h$dl[nu, ])) / 4
+    dds <- dds + (hinv[nu] * h$ddl[nu, , ]) / 2
+  }
+  return(list(ds = ds, dds = dds))
+}
+
+mdfaEigenDerivativesNum <- function(theta) {
+  theFunc <- function(theta, j) {
+    amat <- theA(theta)
+    hval <- eigen(amat, symmetric = TRUE)$values
+    return(hval[j])
+  }
+  g <- matrix(0, m, p)
+  h <- array(0, c(m, p, p))
+  for (nu in 1:m) {
+    g[nu, ] <- grad(theFunc, theta, j = nu, method.args = list(r = 4))
+    h[nu, , ] <- hessian(theFunc, theta, j = nu, method.args = list(r = 4))
+  }
+  return(list(dl = g, ddl = h))
+}
+
+mdfaDerivativesNum <- function(theta) {
+  theFunc <- function(theta) {
+    amat <- theA(theta)
+    hval <- eigen(amat, symmetric = TRUE)$values
+    return(sum(sqrt(hval[1:m])))
+  }
+  ds <- grad(theFunc, theta)
+  dds <- hessian(theFunc, theta)
+  return(list(ds = ds, dds = dds))
+}
+
+mdfaDerivativesAlt <- function(theta, eps = 1e-6) {
+  theFunc <- function(theta) {
+    amat <- theA(theta)
+    hval <- eigen(amat, symmetric = TRUE)$values
+    return(sum(sqrt(hval[1:m])))
+  }
+  f <- theFunc(theta)
+  dds <- matrix(0, p, p)
+  for (s in 1:p) {
+    f1 <- theFunc(theta + eps * ei(s, p))
+    for (t in 1:p) {
+      f2 <- theFunc(theta + eps * ei(t, p))
+      f12 <- theFunc(theta + eps * (ei(t, p) + ei(s, p)))
+      dds[s, t] <- (f12 - f1 - f2 + f) / (eps^2)
+    }
+  }
+  return(dds)
+}
